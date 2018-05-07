b5ba02b81f36964e3fc389651672bba8fe70a709
Crisan Daniel <daniel.proba@yahoo.com>
2018-05-07 22:58:20 +0300

    am facut sa fie unu

--- raul/redirectSearchHomeBtn.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	if (isset($_SESSION['login_user']) && $_SESSION['login_user'] == true) {
-    header("location:indexLogat.php");
-} else {
-   header("location:index.php");
-}
-?>
