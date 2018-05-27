4a42a69ba4b8c434abdf15dbf7ad190d74ce3e27
RaulDobra <rauldobra1@gmail.com>
2018-05-27 23:49:27 +0300

    Introduce capitolele

--- validare.php
+++ validare.php
@@ -6,10 +6,36 @@ if(isset($_POST['butonDa'])){
 	// BUTON DA -- ADAUGARE
 	$id=$_POST['butonDa'];
 	
-	$sql='insert into words (name,short,description) SELECT name,short,description from words_add where words_add.id="'.$id.'"';	
+	
+	
+	$sql='insert into words (name,short,description) SELECT name,short,description from words_add where words_add.id="'.$id.'"';
+	
 $result=mysqli_query($db,$sql);
 	if($result){
 		alert("Cuvant adaugat cu succes !");
+		
+		$ultimul_id = mysqli_insert_id($db);
+		alert($ultimul_id);
+		
+		$capitol=mysqli_query($db,'select capitol_id from words_add where words_add.id="'.$id.'"');
+		$capitol=mysqli_fetch_row($capitol);
+		$capitol=$capitol[0];
+		
+		switch ($capitol){
+			case 10: mysqli_query($db,'insert into capitol_words (capitol_id,word_id) values ("1","'.$ultimul_id.'")');
+					   mysqli_query($db,'insert into capitol_words (capitol_id,word_id) values ("2","'.$ultimul_id.'")');
+					   alert("Introdus in ambele capitole !");
+					   break;
+			 case 1:  mysqli_query($db,'insert into capitol_words (capitol_id,word_id) values ("1","'.$ultimul_id.'")');	
+					   alert("Introdus capitol - IT !");
+					   break;
+					   
+			case 2:  mysqli_query($db,'insert into capitol_words (capitol_id,word_id) values ("2","'.$ultimul_id.'")');	
+					   alert("Introdus capitol - Automatizari !");			
+					   break;
+		  
+			default: alert("Eroare introducere capitol !");
+		}
 		
 		$sql='delete from words_add where words_add.id="'.$id.'"';
 			$result=mysqli_query($db,$sql);
@@ -18,6 +44,8 @@ $result=mysqli_query($db,$sql);
 	}
 	}
 }
+
+
 //BUTON NU -- ADAUGARE
 	if(isset($_POST['butonNu'])){
 	$nume=$_POST['butonNu'];		
